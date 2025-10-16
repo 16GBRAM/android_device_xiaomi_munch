a22d182e10cddef352560b2b82673c92166e1d1b
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-10-16 17:21:28 +0530

    munch:set gapps flag to true

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -16,7 +16,9 @@ $(call inherit-product, device/xiaomi/munch/device.mk)
 
 TARGET_DISABLE_EPPE := true
 MISTOS_MAINTAINER="LordPutin"
-WITH_GMS := false
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_MINI_GAPPS := true
 
 PRODUCT_NAME := mist_munch
 PRODUCT_DEVICE := munch
