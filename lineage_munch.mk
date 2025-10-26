3c850898a52c490980d7f5dc9abee3605cb6ef4c
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-10-26 18:21:36 +0530

    Remove WITH_GAPPS setting from lineage_munch.mk

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -18,7 +18,6 @@ $(call inherit-product, device/xiaomi/munch/device.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 TARGET_DISABLE_EPPE := true
-WITH_GAPPS := true
 TARGET_OPTIMIZED_DEXOPT := true
 BYPASS_CHARGE_SUPPORTED := false
 WITH_BCR := true
