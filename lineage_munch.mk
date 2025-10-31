1ef3d2f98f760aa7b80549665b2527a4ab79469e
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-10-31 16:48:54 +0530

    Disable GMS support in lineage_munch.mk

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/xiaomi/munch/device.mk)
 
 TARGET_DISABLE_EPPE := true
 MISTOS_MAINTAINER="LordPutin"
-WITH_GMS := true
+WITH_GMS := false
 TARGET_ENABLE_BLUR := true
 
 PRODUCT_NAME := mist_munch
